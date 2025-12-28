034753ae4af68011a3a61171e817e08a515e180a
dINESHDF <137500922+dINESHDF@users.noreply.github.com>
2025-12-28 13:04:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const path = require('path');
 const PDFProcessor = require('./api/pdfProcessor');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // In-memory "database"
 const users = [];
